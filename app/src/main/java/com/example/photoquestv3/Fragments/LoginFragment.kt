95bdee8088006f99f123be1e1fa3eb3fae37b4bd
Nicholas <39989194+jeria93@users.noreply.github.com>
2025-02-11 10:00:01 +0100

    feat: Implement navigation from login to feed
    - Added a button click listener to `signInButton` in `LoginFragment`.
    - Implemented navigation to `FeedActivity` upon clicking the sign-in button.
    - Assigned id "signInButton" to the sign in button in fragment_login.xml.

--- app/src/main/java/com/example/photoquestv3/Fragments/LoginFragment.kt
+++ app/src/main/java/com/example/photoquestv3/Fragments/LoginFragment.kt
@@ -1,11 +1,13 @@
 package com.example.photoquestv3.Views.Fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.photoquestv3.R
+import com.example.photoquestv3.Views.FeedActivity
 import com.example.photoquestv3.databinding.FragmentLoginBinding
 
 
@@ -25,10 +27,12 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        binding.signInButton.setOnClickListener {
+            val intent = Intent(requireContext(), FeedActivity::class.java)
+            startActivity(intent)
+        }
 
     }
-
 
     override fun onDestroyView() {
         super.onDestroyView()
