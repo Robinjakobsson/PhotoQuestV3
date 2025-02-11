fdb7e7f55cdbaf6935e3b4efb3f8637f2a099315
Nicholas <39989194+jeria93@users.noreply.github.com>
2025-02-11 13:47:16 +0100

    feat: Implement PostRepository and PostViewModel
    - Created `PostRepository` with methods for post management.
    - Created `PostViewModel` to manage post data using `PostRepository`.

--- app/src/main/java/com/example/photoquestv3/Repositories/PostRepository.kt
+++ app/src/main/java/com/example/photoquestv3/Repositories/PostRepository.kt
@@ -1,6 +1,31 @@
 package com.example.photoquestv3.Repositories
 
+import com.example.photoquestv3.Models.Post
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+
 class PostRepository {
+
+    private val db = Firebase.firestore
+    private val dbStorage = Firebase.storage
+
+    fun addPost(post: Post) {}
+
+    fun uploadPost(post: Post) {}
+
+    fun deletePost(postId: String) {}
+
+    fun updatePost(post: Post) {}
+
+    fun getAllPosts() {}
+
+//    Show all posts by user
+    fun getPostsByUserId(userId: String) {}
+
+//    For future references = Have the ability to see all liked posts in activity or fragment?
+    fun likePost(postId: String) {}
+
 
 
 }
--- app/src/main/java/com/example/photoquestv3/ViewModel/PostViewModel.kt
+++ app/src/main/java/com/example/photoquestv3/ViewModel/PostViewModel.kt
@@ -1,4 +1,18 @@
 package com.example.photoquestv3.ViewModel
 
+import androidx.lifecycle.MutableLiveData
+import com.example.photoquestv3.Models.Post
+import com.example.photoquestv3.Repositories.PostRepository
+
 class PostViewModel {
+
+    private val postRepository = PostRepository()
+
+    private val _posts = MutableLiveData<List<Post>>()
+    val posts: MutableLiveData<List<Post>> = _posts
+
+//    Methods:
+
+
+
 }
