1b3196f1d3d4d1aa81f5af5038e4f5b763140f65
Nicholas <39989194+jeria93@users.noreply.github.com>
2025-02-11 16:03:51 +0100

    feat: Begin implementation of post creation in PostRepository
    - Added initial `createPost` function for creating posts with image upload to Firebase Storage.
    - Added placeholder functions

--- app/src/main/java/com/example/photoquestv3/Repositories/PostRepository.kt
+++ app/src/main/java/com/example/photoquestv3/Repositories/PostRepository.kt
@@ -1,30 +1,48 @@
 package com.example.photoquestv3.Repositories
 
+import android.net.Uri
 import com.example.photoquestv3.Models.Post
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
+import kotlinx.coroutines.tasks.await
+import java.util.UUID
 
 class PostRepository {
 
-    private val db = Firebase.firestore
     private val dbStorage = Firebase.storage
 
-    fun addPost(post: Post) {}
+    suspend fun createPost(
+        imageUri: Uri,
+        description: String,
+        userId: String,
+        userName: String,
+        profileImage: Int
+    ) : String {
 
-    fun uploadPost(post: Post) {}
+        val postId = UUID.randomUUID().toString()
+        val storageRef = dbStorage.reference.child("posts/$postId.jpg")
+        storageRef.putFile(imageUri).await()
+        val imageUrl = storageRef.downloadUrl.await().toString()
 
-    fun deletePost(postId: String) {}
 
-    fun updatePost(post: Post) {}
 
-    fun getAllPosts() {}
+        return  ""
+    }
 
-//    Show all posts by user
-    fun getPostsByUserId(userId: String) {}
+    suspend fun uploadPost(post: Post) {}
 
-//    For future references = Have the ability to see all liked posts in activity or fragment?
-    fun likePost(postId: String) {}
+    suspend fun deletePost(postId: String) {}
+
+    suspend fun updatePost(post: Post) {}
+
+    suspend fun getAllPosts() {}
+
+    //    Show all posts by user
+    suspend fun getPostsByUserId(userId: String) {}
+
+    //    For future references = Have the ability to see all liked posts in activity or fragment?
+    suspend fun likePost(postId: String) {}
 
 
 
