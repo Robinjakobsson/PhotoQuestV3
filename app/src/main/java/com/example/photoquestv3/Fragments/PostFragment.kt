9486afb92f337706dbdb61b64d3a308702f217c9
Nicholas <39989194+jeria93@users.noreply.github.com>
2025-02-11 13:55:37 +0100

    refactor: Migrate PostFragment to View Binding
    - Replaced `findViewById` with viewbinding in `PostFragment`.
    - Initialized binding in `onCreateView` and set it to null in `onDestroyView`.

--- app/src/main/java/com/example/photoquestv3/Fragments/PostFragment.kt
+++ app/src/main/java/com/example/photoquestv3/Fragments/PostFragment.kt
@@ -6,20 +6,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.photoquestv3.R
+import com.example.photoquestv3.databinding.FragmentPostBinding
 
 class PostFragment : Fragment() {
 
+    private var _binding: FragmentPostBinding? = null
+    private val binding get() = _binding!!
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post, container, false)
+        _binding = FragmentPostBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
