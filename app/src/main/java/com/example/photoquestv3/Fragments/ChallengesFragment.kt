a3c9f4e033abce2fb390e731e8568521bb5cc702
camillafalk <ccamilla.falk@gmail.com>
2025-02-12 13:52:58 +0100

    commit.

--- app/src/main/java/com/example/photoquestv3/Fragments/ChallengesFragment.kt
+++ app/src/main/java/com/example/photoquestv3/Fragments/ChallengesFragment.kt
@@ -16,8 +16,13 @@ import com.example.photoquestv3.Adapter.ChallengesRecyclerAdapter
 import com.example.photoquestv3.Models.ChallengeObjects
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.toObject
 
 class ChallengesFragment : Fragment() {
+
+    val db = FirebaseFirestore.getInstance()
+    val collection = db.collection("Challenges")
+    val listOfChallenges = mutableListOf<Challenges>()
 
     companion object {
         fun newInstance() = ChallengesFragment()
@@ -39,32 +44,57 @@ class ChallengesFragment : Fragment() {
     ): View {
         val view = inflater.inflate(R.layout.fragment_challenges, container, false)
 
-         val challenges = mutableListOf(
-             Challenges("Photograph a sunset", "2025-02-15",false),
-             Challenges("Take a picture from an unusual angle","2025-02-14", true),
-             Challenges("Photograph something that symbolizes happiness","2025-02-13", false),
-             Challenges("Take a photo of something in the rain", "2025-02-12",false),
-             Challenges("Photograph something in nature that is reflecting","2025-02-11", false),
-             Challenges("Take a black-and-white photo","2025-02-10", false),
-             Challenges("Take a picture of something in motion", "2025-02-09",false),
-             Challenges("Photograph something old or antique", "2025-02-08",true),
-             Challenges("Take a portrait of someone","2025-02-07", false),
-             Challenges("Take a picture of a reflection", "2025-02-06",false),
-             Challenges("Photograph a flower up close", "2025-02-05",false),
-             Challenges("Take a picture of your own shadow", "2025-02-04",false),
-             Challenges("Photograph something red", "2025-02-11",false),
-             Challenges("Photograph a city skyline at night", "2025-02-03",false),
-             Challenges("Take a picture with a reflective object", "2025-02-02",false),
-             Challenges("Photograph an animal in its natural habitat","2025-02-01", false)
-         )
+        getChallengesFromDatabase()
+
+        /*val challenges = mutableListOf(
+            Challenges("Photograph a sunset", "2025-02-15",false),
+            Challenges("Take a picture from an unusual angle","2025-02-14", true),
+            Challenges("Photograph something that symbolizes happiness","2025-02-13", false),
+            Challenges("Take a photo of something in the rain", "2025-02-12",false),
+            Challenges("Photograph something in nature that is reflecting","2025-02-11", false),
+            Challenges("Take a black-and-white photo","2025-02-10", false),
+            Challenges("Take a picture of something in motion", "2025-02-09",false),
+            Challenges("Photograph something old or antique", "2025-02-08",true),
+            Challenges("Take a portrait of someone","2025-02-07", false),
+            Challenges("Take a picture of a reflection", "2025-02-06",false),
+            Challenges("Photograph a flower up close", "2025-02-05",false),
+            Challenges("Take a picture of your own shadow", "2025-02-04",false),
+            Challenges("Photograph something red", "2025-02-11",false),
+            Challenges("Photograph a city skyline at night", "2025-02-03",false),
+            Challenges("Take a picture with a reflective object", "2025-02-02",false),
+            Challenges("Photograph an animal in its natural habitat","2025-02-01", false)
+        )*/
         val recyclerView = view.findViewById<RecyclerView>(R.id.challengesRecyclerView)
         recyclerView.layoutManager = LinearLayoutManager(context)
 
-        adapter = ChallengesRecyclerAdapter(requireContext(), challenges)
+        adapter = ChallengesRecyclerAdapter(requireContext(), listOfChallenges)
         recyclerView.adapter = adapter
 
         return view
 
+    }
+
+    fun getChallengesFromDatabase() {
+
+        // val today = Calendar.getInstance().time
+        collection
+            .get()
+            .addOnSuccessListener { documentSnapShot ->
+
+                listOfChallenges.clear()
+
+                for (document in documentSnapShot.documents) {
+                    val challenge = document.toObject<Challenges>()
+
+                    if (challenge != null) {
+                        listOfChallenges.add(challenge)
+
+                        Log.d("!!!", "Success fetched challenges and added to list")
+                    }
+                }
+            }.addOnFailureListener { exception ->
+                Log.w("!!!", "Error getting documents: ", exception)
+            }
     }
 
 }
