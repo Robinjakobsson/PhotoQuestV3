dbeac5ab8cb4730eff510ea9c9cede78cc42f781
Nicholas <39989194+jeria93@users.noreply.github.com>
2025-02-12 15:42:24 +0100

    Structure challenges a la MVVM
    - Challenges chain is now by viewmodel,(MVVM)
    - ChallengesFragment had a update, moved around code and delete code that does not have an impact.

--- app/src/main/java/com/example/photoquestv3/Adapter/ChallengesRecyclerAdapter.kt
+++ app/src/main/java/com/example/photoquestv3/Adapter/ChallengesRecyclerAdapter.kt
@@ -10,37 +10,45 @@ import com.example.photoquestv3.Models.Challenges
 import com.example.photoquestv3.R
 
 class ChallengesRecyclerAdapter (
-        private val context: Context,
-        private val challenges: MutableList<Challenges>
-    ) : RecyclerView.Adapter<ChallengesRecyclerAdapter.ViewHolder>() {
+    private val context: Context,
+    private val challenges: MutableList<Challenges>
+) : RecyclerView.Adapter<ChallengesRecyclerAdapter.ViewHolder>() {
 
-        private val layoutInflater = LayoutInflater.from(context)
+    private val layoutInflater = LayoutInflater.from(context)
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            val itemView = layoutInflater.inflate(R.layout.challenges_list_item, parent, false)
-            return ViewHolder(itemView)
-        }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = layoutInflater.inflate(R.layout.challenges_list_item, parent, false)
+        return ViewHolder(itemView)
+    }
 
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            val challenge = challenges[position]
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val challenge = challenges[position]
 
-            //Writes the challenge in the textView.
-            holder.challengesTextView.text = challenge.challenge
-            holder.dateTextView.text = challenge.date
+        //Writes the challenge in the textView.
+        holder.challengesTextView.text = challenge.challenge
+        holder.dateTextView.text = challenge.date
 
-            // Changes the star to yellow if challenge is completed, else, an empty star is shown.
-            if (challenge.completed == true) {
-                holder.starImageView.setImageResource(R.drawable.baseline_star_24)
-            } else {
-                holder.starImageView.setImageResource(R.drawable.baseline_star_outline_24)
-            }
-        }
-       override fun getItemCount(): Int {
-            return challenges.size
-        }
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            val challengesTextView: TextView = itemView.findViewById(R.id.challengesTextView)
-            val starImageView : ImageView = itemView.findViewById(R.id.starImageView)
-        val dateTextView : TextView = itemView.findViewById(R.id.dateTextView)
+        // Changes the star to yellow if challenge is completed, else, an empty star is shown.
+        if (challenge.completed == true) {
+            holder.starImageView.setImageResource(R.drawable.baseline_star_24)
+        } else {
+            holder.starImageView.setImageResource(R.drawable.baseline_star_outline_24)
         }
     }
+    override fun getItemCount(): Int {
+        return challenges.size
+    }
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val challengesTextView: TextView = itemView.findViewById(R.id.challengesTextView)
+        val starImageView : ImageView = itemView.findViewById(R.id.starImageView)
+        val dateTextView : TextView = itemView.findViewById(R.id.dateTextView)
+    }
+
+    fun updateChallenges(newChallenges: List<Challenges>) {
+        challenges.clear()
+        challenges.addAll(newChallenges)
+        notifyDataSetChanged()
+    }
+
+
+}
--- app/src/main/java/com/example/photoquestv3/Fragments/ChallengesFragment.kt
+++ app/src/main/java/com/example/photoquestv3/Fragments/ChallengesFragment.kt
@@ -8,95 +8,58 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.photoquestv3.Models.Challenges
 import com.example.photoquestv3.R
 import com.example.photoquestv3.Adapter.ChallengesRecyclerAdapter
 import com.example.photoquestv3.Models.ChallengeObjects
+import com.example.photoquestv3.ViewModel.ChallengesViewModel
+import com.example.photoquestv3.databinding.FragmentChallengesBinding
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.toObject
 
 class ChallengesFragment : Fragment() {
 
-    val db = FirebaseFirestore.getInstance()
-    val collection = db.collection("Challenges")
+    lateinit var binding: FragmentChallengesBinding
+
     val listOfChallenges = mutableListOf<Challenges>()
-
-    companion object {
-        fun newInstance() = ChallengesFragment()
-    }
-
     lateinit var adapter: ChallengesRecyclerAdapter
+    lateinit var vmChallenges: ChallengesViewModel
 
-    //  private val viewModel: ChallengesViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val view = inflater.inflate(R.layout.fragment_challenges, container, false)
 
-        getChallengesFromDatabase()
+        binding = FragmentChallengesBinding.inflate(inflater, container, false)
+        return binding.root
 
-        /*val challenges = mutableListOf(
-            Challenges("Photograph a sunset", "2025-02-15",false),
-            Challenges("Take a picture from an unusual angle","2025-02-14", true),
-            Challenges("Photograph something that symbolizes happiness","2025-02-13", false),
-            Challenges("Take a photo of something in the rain", "2025-02-12",false),
-            Challenges("Photograph something in nature that is reflecting","2025-02-11", false),
-            Challenges("Take a black-and-white photo","2025-02-10", false),
-            Challenges("Take a picture of something in motion", "2025-02-09",false),
-            Challenges("Photograph something old or antique", "2025-02-08",true),
-            Challenges("Take a portrait of someone","2025-02-07", false),
-            Challenges("Take a picture of a reflection", "2025-02-06",false),
-            Challenges("Photograph a flower up close", "2025-02-05",false),
-            Challenges("Take a picture of your own shadow", "2025-02-04",false),
-            Challenges("Photograph something red", "2025-02-11",false),
-            Challenges("Photograph a city skyline at night", "2025-02-03",false),
-            Challenges("Take a picture with a reflective object", "2025-02-02",false),
-            Challenges("Photograph an animal in its natural habitat","2025-02-01", false)
-        )*/
-        val recyclerView = view.findViewById<RecyclerView>(R.id.challengesRecyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recycleViewSetup()
+
+    }
+
+
+
+    private fun recycleViewSetup() {
+        binding.challengesRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         adapter = ChallengesRecyclerAdapter(requireContext(), listOfChallenges)
-        recyclerView.adapter = adapter
+        binding.challengesRecyclerView.adapter = adapter
 
-        return view
+        vmChallenges = ViewModelProvider(this)[ChallengesViewModel::class.java]
+
+        vmChallenges.challenges.observe(viewLifecycleOwner) { challenges ->
+            adapter.updateChallenges(challenges)
+        }
+        vmChallenges.getChallengesFromDatabase()
     }
 
-    fun getChallengesFromDatabase() {
-
-        // val today = Calendar.getInstance().time
-
-        collection
-            .get()
-            .addOnSuccessListener { documentSnapShot ->
-
-                listOfChallenges.clear()
-
-                for (document in documentSnapShot.documents) {
-                    val challenge = document.toObject<Challenges>()
-
-                    if (challenge != null) {
-                        listOfChallenges.add(challenge)
-
-                        Log.d("!!!", "Success $document")
-                    }
-                }
-                adapter.notifyDataSetChanged()
-
-            }.addOnFailureListener { exception ->
-                Log.w("!!!", "Error getting documents: ", exception)
-            }
-    }
 
 }
