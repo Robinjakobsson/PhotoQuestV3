7928a960692ac63f737190874744cb295b739b50
Nicholas <39989194+jeria93@users.noreply.github.com>
2025-02-13 10:47:28 +0100

    feat: Implement FireStoreViewModel and fetch posts
    - Created `FireStoreViewModel` to fetch post data from the `FireStoreRepository`.
    - Added `fetchPosts` function to retrieve posts from the repository and update the LiveData.
    - Handled potential errors during post retrieval.

--- app/src/main/java/com/example/photoquestv3/ViewModel/FireStoreViewModel.kt
+++ app/src/main/java/com/example/photoquestv3/ViewModel/FireStoreViewModel.kt
@@ -1,4 +1,32 @@
 package com.example.photoquestv3.ViewModel
 
-class FireStoreViewModel {
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.photoquestv3.Models.Post
+import com.example.photoquestv3.Repositories.FireStoreRepository
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.launch
+
+class FireStoreViewModel: ViewModel() {
+
+    val fireStoreDb = FireStoreRepository()
+
+    private val _posts = MutableLiveData<List<Post>>()
+    val posts: MutableLiveData<List<Post>> = _posts
+
+    fun fetchPosts() {
+        viewModelScope.launch {
+            try {
+                val fetchedPosts = fireStoreDb.fetchPostSortedByTime()
+                _posts.postValue(fetchedPosts)
+            } catch (e: Exception) {
+                Log.d("FireStoreViewModel", " [ERROR] Error fetching posts: ${e.message}")
+            }
+        }
+    }
+
+
 }
