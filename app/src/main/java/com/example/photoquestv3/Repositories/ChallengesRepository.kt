47f775d5d3fc0224ad1bdc259b8469260a0e4926
camillafalk <ccamilla.falk@gmail.com>
2025-02-13 11:06:05 +0100

    Added all challenges to each user in database. Code moved back to repository.

--- app/src/main/java/com/example/photoquestv3/Repositories/ChallengesRepository.kt
+++ app/src/main/java/com/example/photoquestv3/Repositories/ChallengesRepository.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.MutableLiveData
 import com.example.photoquestv3.Models.ChallengeObjects
 import com.example.photoquestv3.Models.Challenges
 import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.toObject
 import java.text.SimpleDateFormat
@@ -15,28 +16,24 @@ import java.util.Locale
 class ChallengesRepository {
 
     val db = FirebaseFirestore.getInstance()
-    val collection = db.collection("Challenges")
+    val user = FirebaseAuth.getInstance().currentUser
+    val uid = user?.uid
+    val collection = uid?.let { db.collection("users").document(it) }
 
-
-//    val listOfChallenges = mutableListOf<Challenges>()
 
     val _listOfChallenges = MutableLiveData<List<Challenges>>()
     val listOfChallenges: LiveData<List<Challenges>> get() = _listOfChallenges
 
-
     fun getChallengesFromDatabase() {
 
-        // val today = Calendar.getInstance().time
-
         val today = Calendar.getInstance().time
-
         val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
         val formattedDate = dateFormat.format(today)
 
 
-        collection.whereLessThanOrEqualTo("date", formattedDate)
-            .get()
-            .addOnSuccessListener { documentSnapShot ->
+        collection?.collection("challenges")?.whereLessThanOrEqualTo("date", formattedDate)
+            ?.get()
+            ?.addOnSuccessListener { documentSnapShot ->
 
                 val challengesList = mutableListOf<Challenges>()
 
@@ -53,44 +50,45 @@ class ChallengesRepository {
 
                 _listOfChallenges.value = challengesList
 
-            }.addOnFailureListener { exception ->
+            }?.addOnFailureListener { exception ->
                 Log.w("!!!", "Error getting documents: ", exception)
             }
     }
 
-
-
-    //Does not work (tried in ChallengesFragment). GoogleServices?!?!?!
-    fun doNotUse() {
-
-        val today = Calendar.getInstance().time
-        collection.whereLessThanOrEqualTo("date", today)
-            .get()
-            .addOnSuccessListener { documents ->
-
-//                listOfChallenges.clear()
-
-                for (document in documents) {
-                    val challenge = document.toObject(Challenges::class.java)
-//                    listOfChallenges.add(challenge)
-
-                    Log.d("!!!", "Success fetched challenges and added to list")
-                }
-            }.addOnFailureListener { exception ->
-                Log.w("!!!", "Error getting documents: ", exception)
-            }
-    }
 
     //Works fine to add a list of object from ChallengeObjects to database.(Tested in ChallengesFragment).
     fun addChallengesToDatabase() {
 
-        for (challenge in ChallengeObjects.ChallengeLists) {
-            collection.add(challenge)
-                .addOnSuccessListener {
-                    Log.d("!!!", "challenge added Success")
-                }.addOnFailureListener {
-                    Log.d("!!!", "challenge added Failed.")
+        val db = FirebaseFirestore.getInstance()
+       // val user = FirebaseAuth.getInstance().currentUser
+
+        val userIds = mutableListOf<String>()
+
+        db.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    val uid = document.id
+                    userIds.add(uid)
                 }
-        }
+                Log.d("!!!!", "All userIDs: $userIds")
+
+               // listOfChallenges.clear()
+
+                for (uid in userIds) {
+                    val userDocRef = db.collection("users").document(uid)
+
+                    for (challenge in ChallengeObjects.ChallengeLists) {
+                        userDocRef.collection("challenges").add(challenge)
+                            .addOnSuccessListener {
+                                Log.d("!!!", "challenge added Success")
+                            }.addOnFailureListener {
+                                Log.d("!!!", "challenge added Failed.")
+                            }
+                    }
+                }
+            }.addOnFailureListener { exception ->
+                Log.d("!!!", "Error getting documents: $exception")
+            }
     }
 }
