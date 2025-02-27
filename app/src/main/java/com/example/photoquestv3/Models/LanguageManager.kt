67418b2bee322526afbcf839d7c9081859da3cfa
Nicholas <39989194+jeria93@users.noreply.github.com>
2025-02-27 16:08:21 +0100

    feat: Add LanguageManager
    - Created `LanguageManager` class with a `setLanguage` function.
    - Updates app configuration to switch language at runtime.
    - Forces activity recreation to apply the new locale.

--- app/src/main/java/com/example/photoquestv3/Models/LanguageManager.kt
+++ app/src/main/java/com/example/photoquestv3/Models/LanguageManager.kt
@@ -1,9 +1,23 @@
 package com.example.photoquestv3.Models
 
+import android.media.audiofx.DynamicsProcessing.Config
+import androidx.appcompat.app.AppCompatActivity
+import androidx.work.Configuration
+import java.util.Locale
+
 class LanguageManager {
 
     companion object {
 
+        fun setLanguage(activity: AppCompatActivity, language: String) {
+            val setLang = Locale(language)
+            Locale.setDefault(setLang)
+
+            val config = android.content.res.Configuration(activity.resources.configuration)
+            config.setLocale(setLang)
+            activity.baseContext.resources.updateConfiguration(config, activity.baseContext.resources.displayMetrics)
+            activity.recreate()
+        }
 
     }
 
