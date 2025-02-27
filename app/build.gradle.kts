72c468071ad36a87f6ef04a668a5647817bdb471
Nicholas <39989194+jeria93@users.noreply.github.com>
2025-02-27 11:36:11 +0100

    Upgrade Gradle and Dependencies
    - Working version 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,3 @@
-import org.gradle.internal.impldep.com.fasterxml.jackson.core.JsonPointer.compile
 
 plugins {
     alias(libs.plugins.android.application)
@@ -43,39 +42,39 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.lifecycle.livedata.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.glide)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.firebase.common.ktx)
-    implementation(libs.firebase.firestore.ktx)
-    implementation(libs.firebase.auth.ktx)
-    implementation(libs.firebase.firestore)
-    implementation(libs.firebase.storage)
-    implementation(libs.firebase.auth)
-    implementation(libs.play.services.auth)
-    implementation(libs.androidx.credentials)
-    implementation(libs.androidx.credentials.play.services.auth)
-    implementation(libs.googleid)
+        implementation(libs.androidx.lifecycle.livedata.ktx)
+        implementation(libs.androidx.lifecycle.runtime.ktx)
+        implementation(libs.glide)
+        implementation(libs.androidx.core.ktx)
+        implementation(libs.androidx.appcompat)
+        implementation(libs.material)
+        implementation(libs.androidx.activity)
+        implementation(libs.androidx.constraintlayout)
+        implementation(libs.firebase.common.ktx)
+        implementation(libs.firebase.firestore.ktx)
+        implementation(libs.firebase.auth.ktx)
+        implementation(libs.firebase.firestore)
+        implementation(libs.firebase.storage)
+        implementation(libs.firebase.auth)
+        implementation(libs.play.services.auth)
+        implementation(libs.androidx.credentials)
+        implementation(libs.androidx.credentials.play.services.auth)
+        implementation(libs.googleid)
+        implementation(libs.androidx.work.runtime.ktx)
 
-    //no idea what this is, found in some video about google auth
-//    implementation(libs.spots.dialog)
-  //  implementation ("com.github.d-max:spots-dialog:1.1@aar")
+        testImplementation(libs.junit)
+        androidTestImplementation(libs.androidx.junit)
+        androidTestImplementation(libs.androidx.espresso.core)
 
-    // implementation (libs.material.v1110)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+        implementation(libs.facebook.login.vlatestrelease)
+        implementation(libs.lottie)
 
+        implementation(libs.retrofit)
+        implementation(libs.converter.gson)
 
-    implementation(libs.facebook.login.vlatestrelease)
+        implementation(libs.kotlinx.coroutines.core)
+        implementation(libs.kotlinx.coroutines.android)
 
-    //  Lottie splash screen
-    implementation(libs.lottie)
 
 
 }
